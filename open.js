8894a0e3470e28a463a9ac2a66cfd14aa98793bf
samiMk16 <samiuelmichael@gmail.com>
2022-01-29 21:40:56 -0800

    update on files

---
+++ open.js
@@ -0,0 +1,12 @@
+let numberOfSeasons = 6;
+let numberOfEpisodes = 12;
+//create you variables here
+console.log(numberOfSeasons + numberOfEpisodes);
+
+// let paragraph = document.querySelector('#info');
+// paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
+// let
+let totalLcds =67;
+let totalVinyls = 34;
+let totalMusic = (totalLcds + totalVinyls);
+
